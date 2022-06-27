84904b511733217cc2072eb34d72b3cafe7008e3
Jules <unnamedrealhero@gmail.com>
2022-06-27 14:20:46 +0200

    Quit button

--- Assets/Code/Check.cs
+++ Assets/Code/Check.cs
@@ -5,21 +5,49 @@ using UnityEngine;
 
 public class Check : MonoBehaviour
 {
+    /*
+    Is vase touching the making station?
+    Is there a flower?
+    Is vase correct?
+    Is flower correct?
+1. Have it checked f flower and vase are on the station
+2. Have it chcecked if its the correct flower and vase
+3. Have it define which order is the current one (without changing scenes)
+4. Make after the order gain points.
+5. Clean the making station
+6. Prefabs are not  rigid body unless  touched
+7. Display the text on UI
+8. Change text on the UI
+9. Make an exit button
+
+Checking order
+after
+4,5,8
+Before
+1,2,3
+Other scri[ts
+6,9]
+
+    */
+    public bool IsVaseOnMakingStation;
+    public bool IsThereaFlower;
+    
+    
     
     void Start()
     {
-       //FirstOrder();
+       //Setup FirstOrder();
     }
     public void OnTriggerEnter(Collider other)
     {
          if (other.CompareTag("VaseTag"))
             { 
-                Debug.Log("done");
+                IsVaseOnMakingStation = true;
+                Debug.Log("True");
             }
     }
     public void FirstOrder()
     {
-        
         //blue, brown
     }
 
