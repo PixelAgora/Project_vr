7a7be9dd4db042841365bc07f1ae33863244d51b
Aiden <aiden.gawron@gmail.com>
2022-07-22 14:31:14 +0200

    Better coliders

--- Assets/Code/VaseRaycast.cs
+++ Assets/Code/VaseRaycast.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+/*using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -32,4 +32,4 @@ public class VaseRaycast : MonoBehaviour
             Debug.Log("Did not Hit");
         }
     }
-}
+}*/
